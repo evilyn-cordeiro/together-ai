c820890626ab6097f18bc9364c29c5eab6833f34
evilyn-cordeiro <evilyndeveloper@gmail.com>
2025-03-26 12:35:24 -0300

    fix: ajustei tag de imagem

--- src/app/components/Card/index.tsx
+++ src/app/components/Card/index.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface CardProps {
   icon: string;
   title: string;
@@ -10,7 +12,7 @@ const Card: React.FC<CardProps> = ({ icon, title, description, className }) => {
     <div
       className={`flex flex-col items-start text-left max-w-[450px] ${className}`}
     >
-      <img src={icon} alt={title} className="w-16 h-16 mb-4" />
+      <Image src={icon} alt={title} className="mb-4" width={65} height={65} />
       <h3 className="text-[28px] font-normal  mb-4 tracking-[-.02em] leading-[1.2]">
         {title}
       </h3>
